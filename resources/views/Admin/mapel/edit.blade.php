a0b532c63aeffbcaf8a83ebb4b923a48041d5cc9
kusuma_FE <idabagustrikusumadewa@gmail.com>
2025-06-02 09:37:56 +0800

    ruang dan mapel

---
+++ resources/views/Admin/mapel/edit.blade.php
@@ -0,0 +1,34 @@
+<x-dashboard-layout>
+    <div class="flex flex-row items-center">
+        <a href="" class="breadcrumbs-inactive">Mata Pelajaran</a>
+        <h1 class="breadcrumbs-inactive">/</h1>
+        <h1 class="breadcrumbs-active">Edit</h1>
+    </div>
+
+    <div class="box-dashboard">
+        <form action="" method="POST">
+            @csrf
+
+            @if ($errors->any())
+                <div class="p-3 rounded-md text-red-500 border border-red-500 bg-[#ef44441a] mb-5">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>Error: {{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <div class="input-container !grid-cols-1">
+                <div class="input-group">
+                    <x-label for="name">Nama Mata Pelajaran</x-label>
+                    <x-input id="name" type="text" :disabled="false" name="name" value="Pemrograman Web"
+                        placeholder="Enter member name..."></x-input>
+                </div>
+            </div>
+            <div class="flex flex-row gap-3">
+                <a href="" class="button-secondary" type="submit">Cancel</a>
+                <button class="button-primary" type="submit">Confirm</button>
+            </div>
+        </form>
+    </div>
+</x-dashboard-layout>
---
+++ resources/views/Admin/ruang/index.blade.php
@@ -0,0 +1,57 @@
+<x-dashboard-layout>
+
+    <div class="flex flex-row justify-between items-center">
+        <h1 class="page-title">Ruang</h1>
+        <a href="" class="py-2 px-4 rounded-md bg-blue-600 text-white flex flex-row gap-1 items-center"><i
+                class="ri-add-line"></i></i>Buat Baru</a>
+        {{-- <a href="{{ route('member.archive') }}"
+            class="py-2 px-4 rounded-md bg-blue-600 text-white flex flex-row gap-1 items-center"><i
+                class="ri-add-line"></i>Archive</a> --}}
+    </div>
+
+    <div class="box-dashboard">
+
+        <div class="w-full overflow-x-auto">
+            <table>
+                <thead>
+                    <th class="thead-cell rounded-tl-xl">#</th>
+                    <th class="thead-cell">Nama Ruangan</th>
+                    <th class="thead-cell rounded-tr-xl">Action</th>
+                </thead>
+                <tbody>
+                    {{-- @foreach ($members as $member) --}}
+                    <tr>
+                        {{-- <td class="table-cell">{{ $loop->iteration }} </td> --}}
+                        <td class="table-cell"></td>
+                        <td class="table-cell"></td>
+                        {{-- <td class="table-cell">{{ implode('-', str_split($member->phone_number, 4)) }} </td> --}}
+                        {{-- Untuk No. TElepon --}}
+                        <td class="table-cell w-[20%]">
+                            <div class="flex flex-row gap-3 items-center justify-center">
+                                <a class="text-blue-500" href="">
+                                    <i class="text-base ri-eye-line text-text-secondary-color"></i>
+                                </a>
+                                <a class="text-blue-500" href="">
+                                    <i class="text-base ri-edit-line text-accent-color"></i>
+                                </a>
+                                <form action="" method="post" onsubmit="confirm('Are you sure?')">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit"><i
+                                            class="text-base ri-delete-bin-line text-red-500"></i></button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                    {{-- @endforeach --}}
+                </tbody>
+            </table>
+
+            {{-- @if ($members->isEmpty())
+                <div class="w-full flex justify-center p-2">
+                    <p class="font-satoshi text-text-primary-color">Looks like there's nothing here yet.</p>
+                </div>
+            @endif --}}
+        </div>
+    </div>
+</x-dashboard-layout>
