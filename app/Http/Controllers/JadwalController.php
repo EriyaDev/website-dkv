3f2047ba752eb163563a00031e74df0210800de2
EriyaDev <eriyagamedev@gmail.com>
2025-06-03 12:47:57 +0800

    ruang

--- app/Http/Controllers/JadwalController.php
+++ app/Http/Controllers/JadwalController.php
@@ -30,7 +30,27 @@ class JadwalController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'hari' => 'required|string|max:5',
+            'guru_id' => 'required|string|exists:gurus,id',
+            'kelas_id' => 'required|string|exists:kelass,id',
+            'mapel_id' => 'required|string|exists:mapels,id',
+            'ruang' => 'required|string|exists:ruangs,id',
+            'jam_ke_mulai_id' => 'required|string|exists:jam_pelajarans,id',
+            'jam_ke_selesai_id' => 'required|string|exists:jam_pelajarans,id',
+        ]);
+
+        Jadwal::create([
+            'hari' => $request->hari,
+            'guru_id' => $request->guru_id,
+            'kelas_id' => $request->kelas_id,
+            'mapel_id' => $request->mapel_id,
+            'ruang' => $request->ruang,
+            'jam_ke_mulai_id' => $request->jam_ke_mulai_id,
+            'jam_ke_selesai_id' => $request->jam_ke_selesai_id,
+        ]);
+
+        return redirect()->route('admin.jadwal.create')->with('success', 'Data berhasil dibuat!');
     }
 
     /**
@@ -38,7 +58,7 @@ class JadwalController extends Controller
      */
     public function show(Jadwal $jadwal)
     {
-        //
+        return view('Admin.Jadwal.show', compact('jadwal'));
     }
 
     /**
@@ -46,7 +66,7 @@ class JadwalController extends Controller
      */
     public function edit(Jadwal $jadwal)
     {
-        //
+        return view('Admin.Jadwal.edit', compact('jadwal'));
     }
 
     /**
@@ -54,7 +74,27 @@ class JadwalController extends Controller
      */
     public function update(Request $request, Jadwal $jadwal)
     {
-        //
+        $request->validate([
+            'hari' => 'required|string|max:5',
+            'guru_id' => 'required|string|exists:gurus,id',
+            'kelas_id' => 'required|string|exists:kelass,id',
+            'mapel_id' => 'required|string|exists:mapels,id',
+            'ruang' => 'required|string|exists:ruangs,id',
+            'jam_ke_mulai_id' => 'required|string|exists:jam_pelajarans,id',
+            'jam_ke_selesai_id' => 'required|string|exists:jam_pelajarans,id',
+        ]);
+
+        $jadwal->update([
+            'hari' => $request->hari,
+            'guru_id' => $request->guru_id,
+            'kelas_id' => $request->kelas_id,
+            'mapel_id' => $request->mapel_id,
+            'ruang' => $request->ruang,
+            'jam_ke_mulai_id' => $request->jam_ke_mulai_id,
+            'jam_ke_selesai_id' => $request->jam_ke_selesai_id,
+        ]);
+
+        return redirect()->route('admin.jadwal.create')->with('success', 'Data berhasil dibuat!');
     }
 
     /**
@@ -62,6 +102,8 @@ class JadwalController extends Controller
      */
     public function destroy(Jadwal $jadwal)
     {
-        //
+        $jadwal->delete();
+
+        return redirect()->route('admin.jadwal.index')->with('success', '');
     }
 }
--- database/migrations/2025_06_01_041318_create_ruangs_table.php
+++ database/migrations/2025_06_01_041318_create_ruangs_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('ruangs', function (Blueprint $table) {
             $table->id();
             $table->string('nama_ruang', 50)->unique();
+            $table->string('nama_gedung');
             $table->timestamps();
         });
     }
