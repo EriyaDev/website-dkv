f51811ae9bc410c9b62cc321253e56fac4942e9a
kusuma_FE <idabagustrikusumadewa@gmail.com>
2025-06-03 14:07:08 +0800

    jam pelajaran

---
+++ resources/views/Admin/jam_pelajaran/create.blade.php
@@ -0,0 +1,44 @@
+<x-dashboard-layout>
+    <div class="flex flex-row items-center">
+        <a href="{{ url('admin/jam-pelajaran') }}" class="breadcrumbs-inactive">Jam Pelajaran</a>
+        <h1 class="breadcrumbs-inactive">/</h1>
+        <h1 class="breadcrumbs-active">Buat</h1>
+    </div>
+
+    <div class="box-dashboard">
+        <form action="{{ url('admin/jam-pelajaran') }}" method="POST">
+            @csrf
+
+            @if ($errors->any())
+                <div class="p-3 rounded-md text-red-500 border border-red-500 bg-[#ef44441a] mb-5">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>Error: {{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <div class="input-container">
+                <div class="input-group">
+                    <x-label for="jam_pelajaran_ke">Jam Pelajaran Ke</x-label>
+                    <x-input id="jam_pelajaran_ke" type="number" :disabled="false" name="jam_pelajaran_ke"
+                        value="" placeholder="Masukan jam pelajaran"></x-input>
+                </div>
+                <div class="input-group">
+                    <x-label for="jam_mulai">Jam Mulai</x-label>
+                    <x-input id="jam_mulai" type="time" :disabled="false" name="jam_mulai" value=""
+                        placeholder="Masukan jam mulai"></x-input>
+                </div>
+                <div class="input-group">
+                    <x-label for="jam_selesai">Jam Selesai</x-label>
+                    <x-input id="jam_selesai" type="time" :disabled="false" name="jam_selesai" value=""
+                        placeholder="Masukan jam selesai"></x-input>
+                </div>
+            </div>
+            <div class="flex flex-row gap-3">
+                <a href="{{ url('admin/jam-pelajaran') }}" class="button-secondary" type="submit">Cancel</a>
+                <button class="button-primary" type="submit">Confirm</button>
+            </div>
+        </form>
+    </div>
+</x-dashboard-layout>
---
+++ resources/views/Admin/jam_pelajaran/index.blade.php
@@ -0,0 +1,62 @@
+<x-dashboard-layout>
+
+    <div class="flex flex-row justify-between items-center">
+        <h1 class="page-title">Jam Pelajaran</h1>
+        <a href="{{ url('admin/jam-pelajaran/create') }}"
+            class="py-2 px-4 rounded-md bg-blue-600 text-white flex flex-row gap-1 items-center"><i
+                class="ri-add-line"></i></i>Buat Baru</a>
+        {{-- <a href="{{ route('member.archive') }}"
+            class="py-2 px-4 rounded-md bg-blue-600 text-white flex flex-row gap-1 items-center"><i
+                class="ri-add-line"></i>Archive</a> --}}
+    </div>
+
+    <div class="box-dashboard">
+
+        <div class="w-full overflow-x-auto">
+            <table>
+                <thead>
+                    <th class="thead-cell rounded-tl-xl">#</th>
+                    <th class="thead-cell">Jam Pelajaran Ke</th>
+                    <th class="thead-cell">Jam Mulai</th>
+                    <th class="thead-cell">Jam Selesai</th>
+                    <th class="thead-cell rounded-tr-xl">Action</th>
+                </thead>
+                <tbody>
+                    @foreach ($data as $item)
+                        <tr>
+                            <td class="table-cell">{{ $loop->iteration }} </td>
+                            <td class="table-cell">{{ $item->jam_pelajaran_ke }}</td>
+                            <td class="table-cell">{{ \Carbon\Carbon::parse($item->jam_mulai)->format('H:i') }}</td>
+                            <td class="table-cell">{{ \Carbon\Carbon::parse($item->jam_selesai)->format('H:i') }}</td>
+                            {{-- Untuk No. TElepon --}}
+                            <td class="table-cell w-[20%]">
+                                <div class="flex flex-row gap-3 items-center justify-center">
+                                    <a class="text-blue-500" href="{{ url('admin/jam-pelajaran/' . $item->id) }}">
+                                        <i class="text-base ri-eye-line text-text-secondary-color"></i>
+                                    </a>
+                                    <a class="text-blue-500"
+                                        href="{{ url('admin/jam-pelajaran/' . $item->id . '/edit') }}">
+                                        <i class="text-base ri-edit-line text-accent-color"></i>
+                                    </a>
+                                    <form action="{{ url('admin/jam-pelajaran/' . $item->id) }}" method="POST"
+                                        onsubmit="confirm('Are you sure?')">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit"><i
+                                                class="text-base ri-delete-bin-line text-red-500"></i></button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+            @if ($data->isEmpty())
+                <div class="w-full flex justify-center p-2">
+                    <p class="font-satoshi text-text-primary-color">Looks like there's nothing here yet.</p>
+                </div>
+            @endif
+        </div>
+    </div>
+</x-dashboard-layout>
